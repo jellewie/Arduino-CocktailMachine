6c744ef1612c00398d8cc00bfb29d3f954bdeb7a
JelleWho <1545223+jellewie@users.noreply.github.com>
2025-03-18 18:12:13 +0100

    groundwork for duplex communication
    Duplex communication, adoption, commands, scan ID's, and the

--- CAD/Parts/Dispenser DOUBLENOZZLE/Code bits/DISPENSER/DISPENSER.ino
+++ CAD/Parts/Dispenser DOUBLENOZZLE/Code bits/DISPENSER/DISPENSER.ino
@@ -6,37 +6,38 @@
 #include <FastLED.h>  //Include the libary FastLED (If you get a error here, make sure it's installed!)
 #include <EEPROM.h>
 #include <PJONSoftwareBitBang.h>
+#define SWBB_MODE 1  //Should be default, 1=1.97kB/s //https://github.com/gioblu/PJON/blob/master/src/strategies/SoftwareBitBang/README.md#performance
+//#define SWBB_MAX_ATTEMPTS 20;	//Maximum transmission attempts	Numeric value (20 by default)
 
-const byte DeviceID = 1;  //Unique 1-byte ID (change for every dispenser). must be higher than 0
+const uint8_t PDI_Button = 14;      //Pull down to trigger
+const uint8_t PDO_ValveFluid = 33;  //LOW=OFF
+const uint8_t PDO_ValveAir = 32;    //LOW=OFF
+const uint8_t PDI_SLOT_TXRX = 13;   //The wire from which to get local ID from    //must be 2 or 3 on attiny85 I think
+const uint8_t PAO_LED = 26;         //To which pin the <LED> is connected to
+const uint8_t PDIO_buspin = 25;
 
-const byte PDI_Button = 14;      //Pull down to trigger
-const byte PDO_ValveFluid = 33;  //LOW=OFF
-const byte PDO_ValveAir = 32;    //LOW=OFF
-const byte PDI_SLOT_TXRX = 13;   //The wire from which to get local ID from    //must be 2 or 3 on attiny85 I think
-const byte PAO_LED = 26;         //To which pin the <LED> is connected to
-const byte PDIO_buspin = 25;
-
-const int TotalLEDs = 1;  //The total amounts of LEDs in the strip
+const uint16_t TotalLEDs = 1;  //The total amounts of LEDs in the strip
 CRGB LEDs[TotalLEDs];
 CRGB ColorBoot = CRGB(255, 128, 0);
 CRGB ColorGetID = CRGB(255, 0, 0);
+CRGB ColorWaitForAdoptionIdle = CRGB(255, 255, 0);
 CRGB ColorIdle = CRGB(0, 0, 255);
 CRGB ColorDispencing = CRGB(0, 255, 0);
-byte SlotID = 0;                  //Store the received ID
-byte FluidID = 0;                 //Store the fluid of this dispenser
+uint8_t FluidID = 0;              //Store the fluid of this dispenser
 unsigned long lastPulseTime = 0;  //Time of last revieved pulse from SLOT
-int DelayAir = 100;               //ms to let the air valve open before the fluid valve, to get rid of pressure buildup in the bottle
+uint16_t DelayAir = 100;          //ms to let the air valve open before the fluid valve, to get rid of pressure buildup in the bottle
+bool ImAdopted = false;
 enum COMMANDS { UNK,
                 DISPENSE,
                 GETFLUID,
                 CHANGEFLUID,
                 CHANGEDELAY,
-                CHANGECOLOR };
+                CHANGECOLOR,
+                ADOPT };
 enum COMMAND { DISPENSERUNK,
                DISPENSERSTATUS,
-               DISPENSERFLUID,
-               DISPENSERSLOT };
-PJONSoftwareBitBang bus(DeviceID);  //DeviceID
+};
+PJONSoftwareBitBang bus;  //DeviceID = PJON_NOT_ASSIGNED
 void setup() {
   Serial.begin(115200);
   pinMode(PDI_Button, INPUT_PULLUP);
@@ -68,18 +69,6 @@ void loop() {
 
   bus.receive(50000);  // Receive messages
   bus.update();        // Handle bus updates
-  //Serial.println(".");
-
-  //  if (PrimarySerial.available() >= 3) {// Check if data is available to read
-  //    byte SerialSlotID = PrimarySerial.read();
-  //    byte Serialcmd1 = PrimarySerial.read();
-  //    byte Serialcmd2 = PrimarySerial.read();
-  //    while (PrimarySerial.available()) PrimarySerial.read();   //Trash all the other commands if there any
-  //    if (SerialSlotID == SlotID){ //If it is for us
-  //      byte answer = triggerAction(Serialcmd1, Serialcmd2)
-  //      PrimarySerial.print(answer);
-  //    }
-  //  }
 }
 void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
   if (code == PJON_CONNECTION_LOST) {
@@ -102,48 +91,62 @@ void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info
   Serial.print("Received: ");
   for (uint16_t i = 0; i < length; i++)
     Serial.print(payload[i] + String(" "));
-  Serial.println(); 
+  Serial.println();
 
   if (length == 2) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    int TimeDelay = payload[1] * 10;
-    DispenseStart();
-    delay(TimeDelay);
-    DispenseStop();
-    byte BusSend[] = { DeviceID, DISPENSERSTATUS, true };  //Tell the Primary we have succes
-    bus.send(0x00, &BusSend, sizeof(BusSend));             //Send 0x01 (success) to 0x00 (Primary)
-    digitalWrite(LED_BUILTIN, LOW);                        //Turn off LED
+    uint8_t result = triggerAction(payload[0], payload[1]);
+    Serial.println("called triggerAction with result=" + String(result));
+    uint8_t BusSend[] = { DISPENSERSTATUS, true };            //Reply back we have completed succesfully
+    uint16_t result2 = bus.reply(&BusSend, sizeof(BusSend));  //Send success Primary
+    if (result2 == PJON_FAIL) Serial.print("bus.reply wrong =" + String(result2));
   }
 }
 byte triggerAction(byte cmd1, byte cmd2) {
   /*
-    DISPENSE, Time_in_ms*10.   
-    GETFLUID, IGNORED.         
-    CHANGEFLUID, New_ID        
-    CHANGEDELAY, delay_in_ms between air and fluid   
-    CHANGECOLOR, RRGGBBMM (2 bytes for RGB, where 00=off. MM=01=rainbow)  
+    1=DISPENSE, Time_in_ms*10.   
+    2=GETFLUID, IGNORED.         
+    3=CHANGEFLUID, New_ID        
+    4=CHANGEDELAY, delay_in_ms between air and fluid   
+    5=CHANGECOLOR, RRGGBBMM
   */
   byte r, g, b, m;
-  Serial.print("Action" + String(cmd1) + "triggered with variable " + String(cmd2));
+  Serial.println("Action=" + String(cmd1) + " triggered with variable=" + String(cmd2));
   switch (cmd1) {
     case DISPENSE:
       DispenseStart();
-      delay(cmd2 * 2);
+      delay(cmd2 * 10);
       DispenseStop();
       break;
     case GETFLUID:
       return FluidID;
+      break;
     case CHANGEFLUID:
       FluidID = cmd2;
-      b = map(b, 0, 3, 0, 255);  // Map the 2-bit value to the range 0-255
+      break;
+    case CHANGECOLOR:
+      Serial.println("CHANGECOLOR");
+      r = (cmd2 >> 6) & 0b11;    // Extract bits 7-6 (RR)
+      g = (cmd2 >> 4) & 0b11;    // Extract bits 5-4 (GG)
+      b = (cmd2 >> 2) & 0b11;    // Extract bits 3-2 (BB)
+      r = map(r, 0, 3, 0, 255);  //Map the 2-bit value to the range 0-255
+      g = map(g, 0, 3, 0, 255);  //Map the 2-bit value to the range 0-255
+      b = map(b, 0, 3, 0, 255);  //Map the 2-bit value to the range 0-255
+      Serial.println("r=" + String(r) + " g=" + String(g) + " b=" + String(b));
       fill_solid(&(LEDs[0]), TotalLEDs, CRGB(r, g, b));
-      m = cmd2 & 0x03;  // Mask with 0x03 (00000011)
+      FastLED.show();
+      m = cmd2 & 0x03;  //Mask with 0x03 (00000011)
       if (m == 0b01) {
       }
-    //TODO, we can define color modes
-    //m 00 = just default RGB,
-    //m 01 = rainbow mode
-    //m 1x = reserved for other modesDelayAir
+      //TODO, we can define color modes
+      //m 00 = Auto
+      //m 01 = RGB mode,
+      //m 10 = rainbow mode
+      //m 11 = reserved for other modesDelayAir
+      break;
+    case ADOPT:
+      ImAdopted = true;
+      SetLEDidleColor();
+      break;
     default:
       return 4;
       break;
@@ -153,20 +156,28 @@ byte triggerAction(byte cmd1, byte cmd2) {
 void DispenseStart() {
   Serial.println("DispenseStart");
   fill_solid(&(LEDs[0]), TotalLEDs, ColorDispencing);
+  FastLED.show();
   digitalWrite(PDO_ValveAir, HIGH);
   delay(DelayAir);
   digitalWrite(PDO_ValveFluid, HIGH);
-  FastLED.show();
 }
 void DispenseStop() {
   Serial.println("DispenseStop");
   digitalWrite(PDO_ValveAir, LOW);
   digitalWrite(PDO_ValveFluid, LOW);
-  fill_solid(&(LEDs[0]), TotalLEDs, ColorIdle);
+  SetLEDidleColor();
+}
+void SetLEDidleColor() {
+  if (ImAdopted) {
+    fill_solid(&(LEDs[0]), TotalLEDs, ColorIdle);
+  } else {
+    fill_solid(&(LEDs[0]), TotalLEDs, ColorWaitForAdoptionIdle);
+  }
   FastLED.show();
 }
 void CheckAndGetSlotID() {
-  if (SlotID == 0) {
+  if (bus.device_id() == PJON_NOT_ASSIGNED) {
+    Serial.println("SlotID unknown (" + String(bus.device_id()) + ")");
     if (LEDs[0] != ColorGetID and LEDs[0] != ColorDispencing) {  //If not yet desired color, but do not overwrite ColorDispencing
       fill_solid(&(LEDs[0]), TotalLEDs, ColorGetID);
       FastLED.show();
@@ -177,19 +188,17 @@ void CheckAndGetSlotID() {
     delay(10);
     byte ID3 = GetSlotID();
     if (ID1 > 0 && ID1 == ID2 && ID2 == ID3) {
-      SlotID = ID1;
-      if (LEDs[0] != ColorDispencing) {  //Do not overwrite ColorDispencing
-        fill_solid(&(LEDs[0]), TotalLEDs, ColorIdle);
-        FastLED.show();
-      }
-      Serial.println("SlotID recieved, I am " + String(SlotID));
+      bus.set_id(ID1);
+      if (LEDs[0] != ColorDispencing)  //Do not overwrite ColorDispencing
+        SetLEDidleColor();
+      Serial.println("SlotID recieved, I am " + String(bus.device_id()));
     } else {
       Serial.println("Error in SlotID recieved, ID1=" + String(ID1) + " ID2=" + String(ID2) + " ID3=" + String(ID3));
     }
   }
 }
 byte GetSlotID() {
-  Serial.println(String(millis()) + "GetSlotID");
+  Serial.println(String(millis()) + " GetSlotID");
   pinMode(PDI_SLOT_TXRX, OUTPUT);
   digitalWrite(PDI_SLOT_TXRX, LOW);
   delay(1);
--- CAD/Parts/Dispenser DOUBLENOZZLE/Code bits/PRIMARY/PRIMARY.ino
+++ CAD/Parts/Dispenser DOUBLENOZZLE/Code bits/PRIMARY/PRIMARY.ino
@@ -7,57 +7,29 @@
   1. PRIMARY, This one sends commands to the DISPENSER.
   2. DISPENSER, Does all the action of dispensing the liquid and such. connected to both PRIMARY to recieve commands from and SLOT to recieve its ID from
   3. SLOT, this is just a secondary giving the DISPENSER an ID. This ID is used to talk to the PRIMARY
-
-  PCB General rules:
-    The board is designed with SMD parts on only 1 side
-    The board has fiducials so it is PnP compatable
-    The board shoud have labels at each THT pin to make debugging easier
-  PCB DISPENSER:
-    Magnet pins from the SLOT to the DISPENSER are defined as:
-      [24V] [GND] [PRIMARY_RTX] [] [SLOT_TXRX] (This order)
-    The DISPENSER PCB has an ATTiny85 chip that has these Pins
-      [5V] [GND] [Button] [ValveFluid] [PDO_ValveAir] and [SLOT_TXRX] [LED] [PRIMARYTX] [PRIMARYRX]
-    The board converts 24V to 5V for the chip
-    The output to the valves must be done with an transistor or sorts, and will need to be gound switching
-    The button is an SMD internal part, but has jumpoutpins to be external. The button will be debounce by hardware
-    SLOT_TXRX 1M down
-  PCB SLOT:
-    The SLOT PCB has an ATTiny85 chip that has these Pins
-      [5V] [GND] [SLOT_TXRX]
-    The board imports AND exports these pins from other SLOTs/the PRIMARY
-      [24V] [GND] [5V] [PRIMARY_RTX] []
-    SLOT_TXRX  10k up, 660 in line
-  PCB PRIMARY:
-    Exports these pins (Its cheaper to have one 5V power supply, so import this)
-      [24V] [GND] [5V] [PRIMARY_RTX] []
-    PRIMARY_RTX 150 in line, then 10k down
-
-
-  
-  <Dispensor ID> To remember what dispenser is what. can also be Fluid ID at the same time, but its nice to have them seperated
-    <Slot ID> to know wher to move glass to
-    <Fluid ID> What drink is settup
-
 */
 #include <PJONSoftwareBitBang.h>
+#define SWBB_MODE 1  //Should be default, 1=1.97kB/s //https://github.com/gioblu/PJON/blob/master/src/strategies/SoftwareBitBang/README.md#performance
+//#define SWBB_MAX_ATTEMPTS 20;	//Maximum transmission attempts	Numeric value (20 by default)
 
-PJONSoftwareBitBang bus(0);  //Master Device ID
+const uint8_t PrimaryID = 254;
+PJONSoftwareBitBang bus(PrimaryID);            //Master Device ID
+const uint8_t MAX_Dispensers = 253;            //Maximum possible IDs
+uint8_t Dispensers[MAX_Dispensers] = { 250 };  //Array of available DISPENSER IDs
+bool SlotConnected[MAX_Dispensers];
+uint8_t availableCount = 0;     //Number of detected devices
+const uint8_t PDI_Button = 33;  //Button pin with internal pull-up
+const uint8_t PDIO_buspin = 25;
 
-const byte MAX_Dispensers = 255;          //Maximum possible IDs
-byte Dispensers[MAX_Dispensers] = { 1 };  //Array of available DISPENSER IDs
-byte availableCount = 0;                  //Number of detected devices
-const byte PDI_Button = 33;               //Button pin with internal pull-up
-const byte PDIO_buspin = 25;
 enum COMMANDS { UNK,
                 DISPENSE,
                 GETFLUID,
                 CHANGEFLUID,
                 CHANGEDELAY,
-                CHANGECOLOR };
+                CHANGECOLOR,
+                ADOPT };
 enum COMMAND { DISPENSERUNK,
-               DISPENSERSTATUS,
-               DISPENSERFLUID,
-               DISPENSERSLOT };
+               DISPENSERSTATUS,};
 
 void setup() {
   Serial.begin(115200);
@@ -67,45 +39,93 @@ void setup() {
   bus.set_receiver(receiver_function);
   bus.strategy.set_pin(PDIO_buspin);
   bus.begin();
+  pingAll();
 }
 
 void loop() {
-  bus.receive(50000);  //Receive messages
-  bus.update();        //Handle bus updates
+  int result = bus.receive(50000);  //Stop and receive commands for x microseconds (1000 micro = 1ms)
+  if (result != PJON_ACK and result != PJON_FAIL) {
+    /* https://github.com/gioblu/PJON/blob/master/documentation/data-reception.md
+      receive returns the following values:
+      PJON_ACK (6) if a correct reception occurred
+      PJON_NAK (21) if a mistake is found in CRC
+      PJON_BUSY (666) if a transmission for other devices is occurring
+      PJON_FAIL (65535) if no data is received
+    */
+    Serial.println("ERROR bus.receive " + String(result));
+  }
+
+  bus.update();  //Handle bus updates
 
   bool button_state = digitalRead(PDI_Button);
   static bool OLD_button_state = button_state;
   if (button_state != OLD_button_state) {  //Only update if state changes
     OLD_button_state = button_state;
     if (button_state == LOW) {  //If button is pressed (LOW due to internal pull-up)
-      byte DispenserID = Dispensers[0];
 
-      byte BusSend[] = { DISPENSE, 100 };
-      bus.send(DispenserID, BusSend, sizeof(BusSend));  // Send the array to device 45
-      delay(100);                                       //Make sure the bus has time to be clear
+      uint8_t DispenserID = Dispensers[0];
+      uint8_t BusSend[] = { DISPENSE, 100 };
+
+      uint16_t result = bus.send_packet_blocking(DispenserID, BusSend, sizeof(BusSend));
+      if (result != PJON_ACK) {
+        /* https://github.com/gioblu/PJON/blob/master/documentation/data-transmission.md#send
+          send_packet returns the following values:
+          PJON_ACK (6) if transmission occurred and acknowledgement is received if requested
+          PJON_BUSY (666) if bus is busy
+          PJON_FAIL (65535) if transmission failed
+        */
+        Serial.println("ERROR send_packet_blocking " + String(result));
+      }
     }
   }
 }
-
+void pingAll() {
+  //bus.send_packet(PJON_BROADCAST, "Message for all connected devices.", 34);
+  //if we assume the command is 9 byte (header and such also included) and we run at 1.97kB/s it should take 4.57ms. So for 253 dispensers it could take up to 1.5S to scan all. Thats decent
+  Serial.println("pingAll " + String(MAX_Dispensers));
+  static uint8_t BusSend[] = { ADOPT, 0 };
+  for (uint8_t i = 1; i < MAX_Dispensers; i++) {
+    int result = bus.send_packet(i, BusSend, sizeof(BusSend));  //Tries to send data 1 time. its a blocking function so it will wait here
+    if (result == PJON_ACK) {
+      SlotConnected[i] = true;
+      Serial.println("Dispenser " + String(i) + " Connected");
+    } else {
+      SlotConnected[i] = false;
+      Serial.println("Dispenser " + String(i) + " not connected, code=" + String(result));
+    }
+  }
+}
+void pingOnline() {  //We can call this code periodicaly this will check if all dispensers are still attached
+  Serial.println("pingOnline " + String(MAX_Dispensers));
+  static uint8_t BusSend[] = { ADOPT, 0 };
+  for (uint8_t i = 0; i < MAX_Dispensers; i++) {
+    if (SlotConnected[i] == true) {                                        //Only checks slots that should be connected and respond
+      int result = bus.send_packet_blocking(i, BusSend, sizeof(BusSend));  //Check if they are all still online, to make sure also retry if we fail the first time
+      if (result != PJON_ACK) {
+        SlotConnected[i] = false;
+        Serial.println("Dispenser " + String(i) + " disconnected, code=" + String(result));
+      }
+    }
+  }
+}
 void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
   Serial.print("Received: ");
-  for (uint16_t i = 0; i < length; i++)
+  for (uint16_t i = 1; i < length; i++)
     Serial.print(payload[i] + String(" "));
-  Serial.println(); 
+  Serial.println("From_ID=" + String(packet_info.tx.id));
 
-  if (length == 3) {
+  if (length == 2) {
     //FROM, WHAT, DATA
     switch (payload[0]) {  // Check first byte as a command ID
       case DISPENSERSTATUS:
-        Serial.println("Dispenser " + String(payload[0]) + " gave command DISPENSERSTATUS=" + String(payload[1]));
+        Serial.println("Dispenser " + String(packet_info.tx.id) + " gave command DISPENSERSTATUS=" + String(payload[1]));
         break;
       default:
-        Serial.print("Unknown Command: 0x");
+        Serial.print("Which is an unknown Command");
         break;
     }
   }
 }
-
 void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
   if (code == PJON_CONNECTION_LOST) {
     Serial.print("Connection with device ID ");
