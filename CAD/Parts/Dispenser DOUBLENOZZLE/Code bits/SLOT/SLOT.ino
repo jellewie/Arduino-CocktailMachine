64eb8d1838ea1bab55a2c77291a35db74234fb2a
JelleWho <1545223+jellewie@users.noreply.github.com>
2025-03-22 16:12:08 +0100

    Attiny support
    This code is bebugable on an ESP32, when the compiler detects you are compiling to an ESP it will change the code to debugging.
    
    Also tweaked the timers to be faster, found out that the Attiny does not have a problem with this

--- CAD/Parts/Dispenser DOUBLENOZZLE/Code bits/SLOT/SLOT.ino
+++ CAD/Parts/Dispenser DOUBLENOZZLE/Code bits/SLOT/SLOT.ino
@@ -2,25 +2,40 @@
   Program written by JelleWho https://github.com/jellewie
   Board: Attiny85
     Internal 8Mhz
+  
+  This code is bebugable on an ESP32, when the compiler detects you are compiling to an ESP it will change the code to debugging
 */
-const byte SlotID = 250;  //Unique 1-byte ID (change for every slot for dispenser). must be higher than 0
 
+byte SlotID = 250;  //Unique 1-byte ID (change for every slot for dispenser). Must be higher than 0
+
+#ifdef ESP32
 const byte PDI_Slot_TXRX = 13;
-const byte Delaypulses = 10;
+#else
+const byte PDI_Slot_TXRX = 4;
+#endif
+const byte Delaypulses = 1;
 void setup() {
   pinMode(PDI_Slot_TXRX, OUTPUT);
+#ifdef ESP32
   pinMode(LED_BUILTIN, OUTPUT);
+#else
+  SlotID = SlotID + 1; //IDK seems the ATTiny always does not count the int, so just increase it to compensate
+#endif
 }
 void loop() {
+#ifdef ESP32
   digitalWrite(LED_BUILTIN, LOW);
+#endif
   digitalWrite(PDI_Slot_TXRX, HIGH);
   pinMode(PDI_Slot_TXRX, INPUT);
   delay(10);
-  while (digitalRead(PDI_Slot_TXRX) == HIGH) {}	//Wait until its is pulled low by a dispenser
+  while (digitalRead(PDI_Slot_TXRX) == HIGH) {}  //Wait until its is pulled low by a dispenser
+#ifdef ESP32
   digitalWrite(LED_BUILTIN, HIGH);
+#endif
   pinMode(PDI_Slot_TXRX, OUTPUT);
-  delay(10);
-  for (byte i = 0; i < SlotID; i++) { //Amount of pulses based on SlotID
+  delay(2);
+  for (byte i = 0; i < SlotID; i++) {  //Amount of pulses based on SlotID
     digitalWrite(PDI_Slot_TXRX, LOW);
     delayMicroseconds(Delaypulses);
     digitalWrite(PDI_Slot_TXRX, HIGH);
