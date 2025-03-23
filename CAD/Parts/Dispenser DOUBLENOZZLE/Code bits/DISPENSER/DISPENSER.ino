065580aa6d70457945821585f58078f38acec9d1
JelleWho <1545223+jellewie@users.noreply.github.com>
2025-03-23 10:46:16 +0100

    Made the rainbow sync up on command
    Also a lot of Serial commands have been removed
    to get ready to move to an attiny

--- CAD/Parts/Dispenser DOUBLENOZZLE/Code bits/DISPENSER/DISPENSER.ino
+++ CAD/Parts/Dispenser DOUBLENOZZLE/Code bits/DISPENSER/DISPENSER.ino
@@ -41,7 +41,9 @@ enum COMMANDS { UNK,
 };
 PJONSoftwareBitBang bus;  //DeviceID = PJON_NOT_ASSIGNED
 void setup() {
+#ifdef ESP32
   Serial.begin(115200);
+#endif
   pinMode(PDI_Button, INPUT_PULLUP);
   pinMode(PDO_ValveFluid, OUTPUT);
   pinMode(PDO_ValveAir, OUTPUT);
@@ -56,7 +58,6 @@ void setup() {
   bus.set_receiver(receiver_function);
   bus.strategy.set_pin(PDIO_buspin);
   bus.begin();
-  Serial.println("Start done");
 }
 void loop() {
   CheckAndGetSlotID();  //Try and get the SLOT ID
@@ -72,39 +73,26 @@ void loop() {
   bus.receive(50000);  // Receive messages
   bus.update();        // Handle bus updates
 
-  LEDloop();  //Do the LED update loop if needed, this is for rainbow.
-}
-void LEDloop() {
-  EVERY_N_MILLISECONDS(40) {
-    switch (LEDmode) {
-      case 1:
-        static uint8_t gHue;
-        gHue++;
-        fill_rainbow(&LEDs[0], 1, gHue, 1);
-        FastLED.show();
-        break;
-    }
-  }
+  LEDloop(false);  //Do the LED update loop if needed, this is for rainbow.
 }
 void LoadSettings() {
   EEPROM.begin(16);  //Needed for ESP32
   FluidID = EEPROM.read(0);
   DelayAir = EEPROM.read(1);
   MSperML = EEPROM.read(2);
-  for (uint8_t i = 0; i < 3; i++) {
+#ifdef ESP32
+  for (uint8_t i = 0; i < 3; i++)
     Serial.println("LoadedSetting " + String(i) + "=" + EEPROM.read(i));
-  }
+#endif
 }
 void SaveSettings() {
   EEPROM.write(0, FluidID);
   EEPROM.write(1, DelayAir);
   EEPROM.write(2, MSperML);
   EEPROM.commit();  //Needed for ESP32
-  for (uint8_t i = 0; i < 3; i++) {
-    Serial.println("SavedSetting " + String(i) + "=" + EEPROM.read(i));
-  }
 }
 void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
+#ifdef ESP32
   if (code == PJON_CONNECTION_LOST) {
     Serial.print("Connection with device ID ");
     Serial.print(bus.packets[data].content[0], DEC);
@@ -120,60 +108,63 @@ void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
     Serial.print("Content is too long, length: ");
     Serial.println(data);
   }
+#endif
 }
 void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
+#ifdef ESP32
   Serial.print("Received: ");
   for (uint16_t i = 0; i < length; i++)
     Serial.print(payload[i] + String(" "));
   Serial.println();
+#endif
   if (length == 2) {
-    uint8_t result = triggerAction(payload[0], payload[1]);
-    Serial.println("called triggerAction with result=" + String(result));
-    uint8_t BusSend[] = { DISPENSERSTATUS, result };          //Reply back we have completed
+    uint8_t triggerresult = triggerAction(payload[0], payload[1]);
+    uint8_t BusSend[] = { DISPENSERSTATUS, triggerresult };   //Reply back we have completed
     uint16_t result2 = bus.reply(&BusSend, sizeof(BusSend));  //Send success Primary
-    if (result2 == PJON_FAIL) Serial.print("bus.reply wrong =" + String(result2));
+#ifdef ESP32
+    Serial.println("called triggerAction with result=" + String(triggerresult));
+    if (result2 == PJON_FAIL)
+      Serial.print("bus.reply wrong =" + String(result2));
+#endif
   }
 }
 uint8_t triggerAction(uint8_t cmd1, uint8_t cmd2) {
   /*
-    1=DISPENSE, Time_in_ms*100
-    2=GETFLUID, IGNORED
-    3=CHANGEFLUID, New_ID
-    4=CHANGEDELAY, Time_in_ms*5 between air and fluid   
-    5=CHANGECOLOR, RRGGBBMM
+    1=ADOPT, IGNORED
+    2=DISPENSE, ml to dispense
+    3=CALIBRATEMSPERML, MSperML
+    4=CHANGEFLUID, New_Fluid_ID
+    5=CHANGEDELAY, Time_in_ms*5 between air and fluid   
+    6=CHANGECOLOR, RRGGBBMM MM=00 = Auto
+                            MM=01 = RGB mode,
+                            MM=10 = rainbow mode
+                            MM=11 = reserved for other modes
   */
   uint8_t r, g, b, m;
-  Serial.println(FluidID);
   switch (cmd1) {
     case ADOPT:
-      Serial.println("ADOPT");
       ImAdopted = true;
-      SetLEDidleColor();
+      LEDloop(true);
       return FluidID;
       break;
     case DISPENSE:
-      Serial.println("DISPENSE for " + String(cmd2) + "*" + String(MSperML) + "=" + String(cmd2 * MSperML) + "ms");
       DispenseStart();
       delay(cmd2 * MSperML);
       DispenseStop();
       break;
     case CALIBRATEMSPERML:
-      Serial.println("CALIBRATEMSPERML");
       MSperML = cmd2;
       SaveSettings();
       break;
     case CHANGEFLUID:
-      Serial.println("CHANGEFLUID");
       FluidID = cmd2;
       SaveSettings();
       break;
     case CHANGEDELAY:
-      Serial.println("CHANGEDELAY");
       DelayAir = cmd2 * 5;
       SaveSettings();
       break;
     case CHANGECOLOR:
-      Serial.println("CHANGECOLOR");
       r = (cmd2 >> 6) & 0b11;    // Extract bits 7-6 (RR)
       g = (cmd2 >> 4) & 0b11;    // Extract bits 5-4 (GG)
       b = (cmd2 >> 2) & 0b11;    // Extract bits 3-2 (BB)
@@ -181,23 +172,20 @@ uint8_t triggerAction(uint8_t cmd1, uint8_t cmd2) {
       g = map(g, 0, 3, 0, 255);  //Map the 2-bit value to the range 0-255
       b = map(b, 0, 3, 0, 255);  //Map the 2-bit value to the range 0-255
       m = cmd2 & 0x03;           //Mask with 0x03 (00000011)
-      if (m == 0b00) {
-        SetLEDidleColor();
+      if (m == 0) {
+        LEDmode = 0;
+        LEDloop(true);
       }
-      if (m == 0b01) {
+      if (m == 1) {
         fill_solid(&(LEDs[0]), TotalLEDs, CRGB(r, g, b));
         FastLED.show();
       }
-      if (m == 0b10) {
-        if (LEDs[0] == ColorIdle)  //Do not overwrite others, only listen in normal idle state
-          LEDmode = 1;
-      }
+      if (m == 2)
+        if (LEDs[0] == ColorIdle or LEDmode == 1) {  //Do not overwrite other modes OR if we want to sync the rainbow
+          LEDmode = 1;                               //Set rainbow mode
+          LEDloop(true);
+        }
       Serial.println("r=" + String(r) + " g=" + String(g) + " b=" + String(b) + " m=" + String(m));
-      //TODO, we can define color modes
-      //m 00 = Auto
-      //m 01 = RGB mode,
-      //m 10 = rainbow mode
-      //m 11 = reserved for other modesDelayAir
       break;
     default:
       return false;
@@ -206,6 +194,7 @@ uint8_t triggerAction(uint8_t cmd1, uint8_t cmd2) {
   return true;
 }
 void DispenseStart() {
+  LEDmode = 0;
   Serial.println("DispenseStart");
   fill_solid(&(LEDs[0]), TotalLEDs, ColorDispencing);
   FastLED.show();
@@ -217,16 +206,31 @@ void DispenseStop() {
   Serial.println("DispenseStop");
   digitalWrite(PDO_ValveAir, LOW);
   digitalWrite(PDO_ValveFluid, LOW);
-  SetLEDidleColor();
+  LEDloop(true);
 }
-void SetLEDidleColor() {
-  LEDmode = 0;
-  if (ImAdopted) {
-    fill_solid(&(LEDs[0]), TotalLEDs, ColorIdle);
-  } else {
-    fill_solid(&(LEDs[0]), TotalLEDs, ColorWaitForAdoptionIdle);
+void LEDloop(bool init) {
+  static uint8_t gHue;
+  switch (LEDmode) {
+    case 0:
+      if (init) {
+        if (ImAdopted) {
+          fill_solid(&(LEDs[0]), TotalLEDs, ColorIdle);
+        } else {
+          fill_solid(&(LEDs[0]), TotalLEDs, ColorWaitForAdoptionIdle);
+        }
+        FastLED.show();
+      }
+      break;
+    case 1:
+      if (init)
+        gHue = 0;  //Reset gHue offset
+      EVERY_N_MILLISECONDS(40) {
+        gHue++;
+        fill_rainbow(&LEDs[0], 1, gHue, 1);
+        FastLED.show();
+      }
+      break;
   }
-  FastLED.show();
 }
 void CheckAndGetSlotID() {
   if (bus.device_id() == PJON_NOT_ASSIGNED) {
@@ -248,7 +252,7 @@ void CheckAndGetSlotID() {
       uint8_t BusSend[] = { ADOPT, bus.device_id() };  //Ask Primary for us to be adopted
       uint16_t result = bus.send(PrimaryID, &BusSend, sizeof(BusSend));
       //if (result == PJON_FAIL) Serial.print("bus request fail =" + String(result));
-    } 
+    }
     //else {Serial.println("Error in SlotID recieved, ID1=" + String(ID1) + " ID2=" + String(ID2) + " ID3=" + String(ID3));}
   }
 }
