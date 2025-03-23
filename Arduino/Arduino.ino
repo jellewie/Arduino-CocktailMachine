eeafaa99ab74728ca2cbf7b6d69063ad5979394c
JelleWho <1545223+jellewie@users.noreply.github.com>
2025-03-23 16:03:35 +0100

    Update since we stay on ESP32

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -7,35 +7,35 @@
 #error "Please check if the 'DOIT ESP32 DEVKIT V1' board is selected, which can be downloaded at https://dl.espressif.com/dl/package_esp32_index.json"
 #endif
 
-#include "WiFiManagerBefore.h"                                  //Define what options to use/include or to hook into WiFiManager
-#include "WiFiManager/WiFiManager.h"                            //Includes <WiFi> and <WebServer.h> and setups up 'WebServer server(80)' if needed
+#include "WiFiManagerBefore.h"        //Define what options to use/include or to hook into WiFiManager
+#include "WiFiManager/WiFiManager.h"  //Includes <WiFi> and <WebServer.h> and setups up 'WebServer server(80)' if needed
 //#include FastLED>
-#include <AccelStepper.h>                                       //Make sure to install AccelStepper V1.61.0(+) manually //https://www.airspayce.com/mikem/arduino/AccelStepper/classAccelStepper.html#a68942c66e78fb7f7b5f0cdade6eb7f06
+#include <AccelStepper.h>  //Make sure to install AccelStepper V1.61.0(+) manually //https://www.airspayce.com/mikem/arduino/AccelStepper/classAccelStepper.html#a68942c66e78fb7f7b5f0cdade6eb7f06
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>                                  //Make sure to install LiquidCrystal_I2C V1.1.2(+) manually https://github.com/johnrickman/LiquidCrystal_I2C/blob/master/LiquidCrystal_I2C.cpp
-#include <FastLED.h>                                            //Include the libary FastLED (If you get a error here, make sure it's installed!)
+#include <LiquidCrystal_I2C.h>  //Make sure to install LiquidCrystal_I2C V1.1.2(+) manually https://github.com/johnrickman/LiquidCrystal_I2C/blob/master/LiquidCrystal_I2C.cpp
+#include <FastLED.h>            //Include the libary FastLED (If you get a error here, make sure it's installed!)
 //==============================================================
 //Very custom user settings
 //==============================================================
 const byte PDO_Step_enable = 16;
-const byte PAO_LED = 13;                                        //To which pin the <LED strip> is connected to
+const byte PAO_LED = 13;  //To which pin the <LED strip> is connected to
 const byte PDO_X_Dir = 23;
 const byte PDO_Y_Dir = 17;
 const byte PDO_Z_Dir = 18;
 const byte PDO_X_Step = 4;
 const byte PDO_Y_Step = 12;
 const byte PDO_Z_Step = 19;
-const byte PDI_X_Ref = 5;                                       //LOW = TRIGGERED
+const byte PDI_X_Ref = 5;  //LOW = TRIGGERED
 const byte PDI_Y_Ref = 27;
 const byte PDI_Z_Ref = 15;
 const byte PDI_S = 39;
-const byte PDO_Pump[] = {32, 33, 25, 26};
-const int TotalLEDs = 100;                                      //The total amounts of LEDs in the strip
-CRGB ColorBoot       = CRGB(255, 128,   0);
-CRGB ColorHoming     = CRGB(  0,   0, 255);
-CRGB ColorHomeFail   = CRGB(255,   0,   0);
-CRGB ColorMoveBase   = CRGB(  0,   0,   0);
-CRGB ColorMoveActive = CRGB(  0, 255,   0);
+const byte PDO_Pump[] = { 32, 33, 25, 26 };
+const int TotalLEDs = 100;  //The total amounts of LEDs in the strip
+CRGB ColorBoot = CRGB(255, 128, 0);
+CRGB ColorHoming = CRGB(0, 0, 255);
+CRGB ColorHomeFail = CRGB(255, 0, 0);
+CRGB ColorMoveBase = CRGB(0, 0, 0);
+CRGB ColorMoveActive = CRGB(0, 255, 0);
 //==============================================================
 //Soft settings (can be changed with the interface)
 //==============================================================
@@ -61,16 +61,16 @@ bool Homed = false;
 //End of settings
 //==============================================================
 bool UpdateLEDs = false;
-byte Pump_Amount = sizeof(PDO_Pump) / sizeof(PDO_Pump[0]);      //Why filling this in if we can automate that? :)
-const byte Dispensers_Amount = 20;                              //Only 20 are saved in the WiFiManager!!
+byte Pump_Amount = sizeof(PDO_Pump) / sizeof(PDO_Pump[0]);  //Why filling this in if we can automate that? :)
+const byte Dispensers_Amount = 20;                          //Only 20 are saved in the WiFiManager!!
 AccelStepper Stepper_X(AccelStepper::DRIVER, PDO_X_Step, PDO_X_Dir);
 AccelStepper Stepper_Y(AccelStepper::DRIVER, PDO_Y_Step, PDO_Y_Dir);
 AccelStepper Stepper_Z(AccelStepper::DRIVER, PDO_Z_Step, PDO_Z_Dir);
-LiquidCrystal_I2C lcd(0x27, 20, 4);                             //Set the LCD address to 0x27 for a 20 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 20, 4);  //Set the LCD address to 0x27 for a 20 chars and 2 line display
 CRGB LEDs[TotalLEDs];
 #include "Data.h"
 #include "Functions.h"
-#include "WiFiManagerLater.h"                                   //Define options of WiFiManager (can also be done before), but WiFiManager can also be called here (example for DoRequest included here)
+#include "WiFiManagerLater.h"  //Define options of WiFiManager (can also be done before), but WiFiManager can also be called here (example for DoRequest included here)
 
 void setup() {
   Serial.begin(115200);
@@ -84,9 +84,9 @@ void setup() {
   //===========================================================================
   pinMode(PAO_LED, OUTPUT);
   FastLED.addLeds<WS2812B, PAO_LED, GRB>(LEDs, TotalLEDs);
-  FastLED.setBrightness(MaxBrightness);                         //Set brightness
+  FastLED.setBrightness(MaxBrightness);  //Set brightness
   LED_Fill(0, TotalLEDs, ColorBoot);
-  FastLED.show();                                               //Update
+  FastLED.show();  //Update
   //===========================================================================
   pinMode(PDO_Step_enable, OUTPUT);
   DisableSteppers();
@@ -104,7 +104,7 @@ void setup() {
     pinMode(PDO_Pump[i], OUTPUT);
     digitalWrite(PDO_Pump[i], LOW);
   }
-  Stepper_X.setPinsInverted(false, true, false, false, false); //stepInvert, directionInvert, pin3Invert, pin4Invert, enableInvert
+  Stepper_X.setPinsInverted(false, true, false, false, false);  //stepInvert, directionInvert, pin3Invert, pin4Invert, enableInvert
   Stepper_Y.setPinsInverted(false, true, false, false, false);
   Stepper_Z.setPinsInverted(false, true, false, false, false);
   Stepper_X.setMaxSpeed(MotorMAXSpeed);
@@ -116,16 +116,16 @@ void setup() {
   //===========================================================================
   //Set up all server UrlRequest stuff
   //===========================================================================
-  server.on("/",            handle_OnConnect);                  //Call the 'handleRoot' function when a client requests URL "/"
-  server.on("/set",         handle_Set);
-  server.on("/get",         handle_Get);
-  server.on("/info",        handle_Info);
-  server.on("/reset",       handle_Reset);
-  server.onNotFound(        handle_NotFound);                   //When a client requests an unknown URI
+  server.on("/", handle_OnConnect);  //Call the 'handleRoot' function when a client requests URL "/"
+  server.on("/set", handle_Set);
+  server.on("/get", handle_Get);
+  server.on("/info", handle_Info);
+  server.on("/reset", handle_Reset);
+  server.onNotFound(handle_NotFound);  //When a client requests an unknown URI
   //server.on("/url", CodeToCall);                              //Example of a url to function caller, These must be declaired before "WiFiManager.Start()"
-  byte Answer = WiFiManager.Start();                            //Run the wifi startup (and save results)
-  WiFiManager.OTA_Enabled = true;                               //(runtime) Turn off/on OTA
-  WiFiManager.EnableSetup(true);                                //(runtime) Enable the settings, only enabled in APmode by default
+  byte Answer = WiFiManager.Start();  //Run the wifi startup (and save results)
+  WiFiManager.OTA_Enabled = true;     //(runtime) Turn off/on OTA
+  WiFiManager.EnableSetup(true);      //(runtime) Enable the settings, only enabled in APmode by default
   if (Answer == 1) {
     LcdPrint("Mixer online!", IpAddress2String(WiFi.localIP()));
   } else {
@@ -138,7 +138,7 @@ void loop() {
   server.handleClient();
   if (Homed) {
     EVERY_N_MILLISECONDS(40) {
-      LED_Rainbow(0, TotalLEDs, 255 / TotalLEDs);                 //Show a rainbow to sinal we are done and IDLE
+      LED_Rainbow(0, TotalLEDs, 255 / TotalLEDs);  //Show a rainbow to sinal we are done and IDLE
       UpdateLED(true);
     }
   }
@@ -148,14 +148,14 @@ void MakeCocktail(Drink Mix) {
   if (Running) return;
   Running = true;
   LcdPrint("Mixing cocktail", Mix.Name);
-  DisableSteppersinSeconds = -1;                                //Make sure the steppers do not auto disable
+  DisableSteppersinSeconds = -1;  //Make sure the steppers do not auto disable
   if (!Homed) {
     if (!Home(true, true, true)) {
       LcdPrint("Mix not started", "Homing failed");
       return;
     }
   }
-  for (byte i = 0; i < 8; i++) {                                //For each Ingredient
+  for (byte i = 0; i < 8; i++) {  //For each Ingredient
     MyYield();
     if (Mix.Ingredients[i].ID != 0 or Mix.Ingredients[i].Action != 0) {
       String msg = "";
@@ -182,7 +182,8 @@ void GetIngredient(Ingredient IN) {
   if (DispenserID != 255) {
     LightSection(Dispensers[DispenserID].LocationX);
     switch (Dispensers[DispenserID].Type) {
-      case SHOTDISPENSER: {
+      case SHOTDISPENSER:
+        {
           Serial.println("GetIngredient SHOTDispenser IN.ml=" + String(IN.ml) + " ShotDispenserML=" + String(ShotDispenserML));
           MoveTo(Dispensers[DispenserID].LocationX, Dispensers[DispenserID].LocationY);
           int mlToDo = IN.ml;
@@ -199,8 +200,10 @@ void GetIngredient(Ingredient IN) {
             while (Stepper_Z.run()) yield();
             MyDelay(Dispensers[DispenserID].TimeMSoff);
           }
-        } break;
-      case PUMP: {
+        }
+        break;
+      case PUMP:
+        {
           Serial.println("GetIngredient from Pump ID=" + String(Dispensers[DispenserID].LocationZ));
           if (Dispensers[DispenserID].LocationZ <= Pump_Amount) {
             MoveTo(Dispensers[DispenserID].LocationX, Dispensers[DispenserID].LocationY);
@@ -221,10 +224,10 @@ void GetIngredient(Ingredient IN) {
 }
 void LightSection(long LocationX) {
   byte Len = 13;
-  float LEDPos = (LocationX * TotalLEDs ) / (BedSize_X);
+  float LEDPos = (LocationX * TotalLEDs) / (BedSize_X);
   LEDPos = LEDPos - Len < 0 ? 0 : LEDPos - Len;
   LEDPos = LEDPos + Len > TotalLEDs ? LEDPos - Len : LEDPos;
-  LED_Fill(0, TotalLEDs, ColorMoveBase);                        //Set base color
+  LED_Fill(0, TotalLEDs, ColorMoveBase);  //Set base color
   LED_Fill(LEDPos, Len, ColorMoveActive);
   UpdateLED(true);
 }
--- CAD/Parts/Dispenser DOUBLENOZZLE/Code bits/DISPENSER/DISPENSER.ino
+++ CAD/Parts/Dispenser DOUBLENOZZLE/Code bits/DISPENSER/DISPENSER.ino
@@ -1,44 +1,40 @@
 /*
   Program written by JelleWho https://github.com/jellewie
-  Board: Attiny85
+  Board: ESP32
   DISPENSER
 */
-#include <Adafruit_NeoPixel.h>
+#define SerialDebug
+#ifndef ESP32
+#error "Please select ESP32 as a board."
+#endif
+#include <FastLED.h>  //Include the libary FastLED (If you get a error here, make sure it's installed!)
 #include <EEPROM.h>
 #include <PJONSoftwareBitBang.h>
-#define SWBB_MODE 1  //Should be default, 1=1.97kB/s //https://github.com/gioblu/PJON/blob/master/src/strategies/SoftwareBitBang/README.md#performance
+//#define SWBB_MODE 1  //1 should be default, 1=1.97kB/s //https://github.com/gioblu/PJON/blob/master/src/strategies/SoftwareBitBang/README.md#performance
 //#define SWBB_MAX_ATTEMPTS 20;	//Maximum transmission attempts	Numeric value (20 by default)
+const uint8_t PDI_Button = 14;                      //Pull down to trigger
+const uint8_t PDO_ValveFluid = 33;                  //LOW=OFF
+const uint8_t PDO_ValveAir = 32;                    //LOW=OFF
+const uint8_t PDI_SLOT_TXRX = 13;                   //The wire from which to get local ID from    //must be 2 or 3 on attiny85 I think
+const uint8_t PAO_LED = 26;                         //To which pin the <LED> is connected to
+const uint8_t PDIO_buspin = 25;                     //must be 12,25 for SerialDebug
+const uint8_t PrimaryID = 254;                      //Used to request adoption
+const uint8_t TotalLEDs = 1;                        //The total amounts of LEDs in the strip
+CRGB LEDs[TotalLEDs];                               //Array with our status LED
+CRGB ColorBoot = CRGB(255, 128, 0);                 //While starting up
+CRGB ColorGetID = CRGB(255, 0, 0);                  //While trying to get an IP
+CRGB ColorWaitForAdoptionIdle = CRGB(255, 255, 0);  //While waiting for Primary to adopt us
+CRGB ColorIdle = CRGB(0, 0, 255);                   //While idle/normal state
+CRGB ColorDispencing = CRGB(0, 255, 0);             //While dispensing
 
-#ifdef ESP32
-const uint8_t PDI_Button = 14;      //Pull down to trigger
-const uint8_t PDO_ValveFluid = 33;  //LOW=OFF
-const uint8_t PDO_ValveAir = 32;    //LOW=OFF
-const uint8_t PDI_SLOT_TXRX = 13;   //The wire from which to get local ID from    //must be 2 or 3 on attiny85 I think
-const uint8_t PAO_LED = 26;         //To which pin the <LED> is connected to
-const uint8_t PDIO_buspin = 25;     //must be 12,25 for ESP32
-#else
-const uint8_t PDI_Button = 3;      //Pull down button to trigger
-const uint8_t PDO_ValveFluid = 2;  //LOW=OFF
-const uint8_t PDO_ValveAir = 5;    //LOW=OFF
-const uint8_t PDI_SLOT_TXRX = 0;   //The wire from which to get local ID from    //must be 2 or 3 on attiny85 I think
-const uint8_t PAO_LED = 4;         //To which pin the <LED> is connected to
-const uint8_t PDIO_buspin = 1;     //Must be 1/2 for ATtiny85
-#endif
-const uint8_t PrimaryID = 254;                                      //Used to request adoption
-const uint8_t TotalLEDs = 1;                                        //The total amounts of LEDs in the strip
-Adafruit_NeoPixel strip(TotalLEDs, PAO_LED, NEO_GRB + NEO_KHZ800);  //Array with our status LED
-const uint32_t ColorBoot = 0xFF8000;                                //While starting up
-const uint32_t ColorGetID = 0xFF0000;                               //While trying to get an IP
-const uint32_t ColorWaitForAdoptionIdle = 0xFFFF00;                 //While waiting for Primary to adopt us
-const uint32_t ColorIdle = 0x0000FF;                                //While idle/normal state
-const uint32_t ColorDispencing = 0x00FF00;                          //While dispensing
-
-uint8_t FluidID = 0;              //SAVESETTING Store the fluid of this dispenser
-uint8_t MSperML = 40;             //SAVESETTING ms to let 1 ml go, for example it takes 12s to do 300ml, thats about 40 ms per ml
-uint8_t DelayAir = 0;             //SAVESETTING ms to let the air valve open before the fluid valve, to get rid of pressure buildup in the bottle
-unsigned long lastPulseTime = 0;  //Time of last revieved pulse from SLOT
-bool ImAdopted = false;           //If the primary has seen this dispenser yet
-bool LEDrainbow = false;          //use to enable rainbow led mode
+bool ImAdopted = false;   //If the primary has seen this dispenser yet
+bool LEDrainbow = false;  //use to enable rainbow led mode
+struct Settings {
+  uint8_t FluidID;   //Default Store the fluid of this dispenser
+  uint8_t MSperML;   //ms to let 1 ml go, for example it takes 12s to do 300ml, thats about 40 milliseconds per milliliter
+  uint8_t DelayAir;  //ms to let the air valve open before the fluid valve, to get rid of pressure buildup in the bottle
+};
+Settings dispenserSettings;  //Create a variable of type Settings
 enum COMMANDS { UNK,
                 ADOPT,
                 DISPENSE,
@@ -50,7 +46,7 @@ enum COMMANDS { UNK,
 };
 PJONSoftwareBitBang bus;  //DeviceID = PJON_NOT_ASSIGNED
 void setup() {
-#ifdef ESP32
+#ifdef SerialDebug
   Serial.begin(115200);
 #endif
   pinMode(PDI_Button, INPUT_PULLUP);
@@ -60,50 +56,44 @@ void setup() {
   digitalWrite(PDO_ValveFluid, LOW);  //Ensure valve is OFF at start
   digitalWrite(PDO_ValveAir, LOW);    //Ensure valveAir is OFF at start
   LoadSettings();
-  strip.setPixelColor(0, ColorBoot);  //RED to show we do not have an ID
-  strip.begin();
-  strip.show();
+  FastLED.addLeds<WS2812B, PAO_LED, GRB>(LEDs, TotalLEDs);
+  fill_solid(&(LEDs[0]), TotalLEDs, ColorBoot);  //RED to show we do not have an ID
+  FastLED.show();
   bus.set_error(error_handler);
   bus.set_receiver(receiver_function);
   bus.strategy.set_pin(PDIO_buspin);
   bus.begin();
 }
 void loop() {
-  CheckAndGetSlotID();  //Try and get the SLOT ID
-
-  //Manual control
-  bool button_state = digitalRead(PDI_Button);
-  static bool OLD_button_state = button_state;
-  if (button_state != OLD_button_state) {  //Only update if state changes
-    OLD_button_state = button_state;
+  CheckAndGetSlotID();                                         //Try and get the SLOT ID
+  bool button_state = digitalRead(PDI_Button);                 //read manual button
+  static bool OLD_button_state = button_state;                 //Save old state to see changes
+  if (button_state != OLD_button_state) {                      //Only update if button state changes
+    OLD_button_state = button_state;                           //remember the new state as the old one
     (button_state == LOW) ? DispenseStart() : DispenseStop();  //Button pressed
   }
-
-  bus.receive(50000);  // Receive messages
-  bus.update();        // Handle bus updates
-
-  LEDloop(false);  //Do the LED update loop if needed, this is for rainbow.
+  bus.receive(50000);  //Receive messages
+  bus.update();        //Handle bus updates
+  LEDloop(false);      //Do the LED update loop if needed, this is for rainbow.
 }
 void LoadSettings() {
-#ifdef ESP32
+  EEPROM.begin(16);  //Initialize EEPROM (needed for ESP32)
+  dispenserSettings.FluidID = EEPROM.read(0);
+  dispenserSettings.DelayAir = EEPROM.read(1);
+  dispenserSettings.MSperML = EEPROM.read(2);
+#ifdef SerialDebug
   for (uint8_t i = 0; i < 3; i++)
     Serial.println("LoadedSetting " + String(i) + "=" + EEPROM.read(i));
-  EEPROM.begin(16);  //Needed for ESP32
 #endif
-  FluidID = EEPROM.read(0);
-  DelayAir = EEPROM.read(1);
-  MSperML = EEPROM.read(2);
 }
 void SaveSettings() {
-  EEPROM.write(0, FluidID);
-  EEPROM.write(1, DelayAir);
-  EEPROM.write(2, MSperML);
-#ifdef ESP32
-  EEPROM.commit();  //Needed for ESP32
-#endif
+  EEPROM.write(0, dispenserSettings.FluidID);
+  EEPROM.write(1, dispenserSettings.DelayAir);
+  EEPROM.write(2, dispenserSettings.MSperML);
+  EEPROM.commit();  //Commit changes (needed for ESP32)
 }
 void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
-#ifdef ESP32
+#ifdef SerialDebug
   if (code == PJON_CONNECTION_LOST) {
     Serial.print("Connection with device ID ");
     Serial.print(bus.packets[data].content[0], DEC);
@@ -122,7 +112,7 @@ void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
 #endif
 }
 void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
-#ifdef ESP32
+#ifdef SerialDebug
   Serial.print("Received: ");
   for (uint16_t i = 0; i < length; i++)
     Serial.print(payload[i] + String(" "));
@@ -132,7 +122,7 @@ void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info
     uint8_t triggerresult = triggerAction(payload[0], payload[1]);
     uint8_t BusSend[] = { DISPENSERSTATUS, triggerresult };   //Reply back we have completed
     uint16_t result2 = bus.reply(&BusSend, sizeof(BusSend));  //Send success Primary
-#ifdef ESP32
+#ifdef SerialDebug
     Serial.println("called triggerAction with result=" + String(triggerresult));
     if (result2 == PJON_FAIL)
       Serial.print("bus.reply wrong =" + String(result2));
@@ -156,47 +146,47 @@ uint8_t triggerAction(uint8_t cmd1, uint8_t cmd2) {
     case ADOPT:
       ImAdopted = true;
       LEDloop(true);
-      return FluidID;
+      return dispenserSettings.FluidID;
       break;
     case DISPENSE:
       DispenseStart();
-      delay(cmd2 * MSperML);
+      delay(cmd2 * dispenserSettings.MSperML);
       DispenseStop();
       break;
     case CALIBRATEMSPERML:
-      MSperML = cmd2;
+      dispenserSettings.MSperML = cmd2;
       SaveSettings();
       break;
     case CHANGEFLUID:
-      FluidID = cmd2;
+      dispenserSettings.FluidID = cmd2;
       SaveSettings();
       break;
     case CHANGEDELAY:
-      DelayAir = cmd2;
+      dispenserSettings.DelayAir = cmd2;
       SaveSettings();
       break;
     case CHANGECOLOR:
-      // r = (cmd2 >> 6) & 0b11;    // Extract bits 7-6 (RR)
-      // g = (cmd2 >> 4) & 0b11;    // Extract bits 5-4 (GG)
-      // b = (cmd2 >> 2) & 0b11;    // Extract bits 3-2 (BB)
-      // r = map(r, 0, 3, 0, 255);  //Map the 2-bit value to the range 0-255
-      // g = map(g, 0, 3, 0, 255);  //Map the 2-bit value to the range 0-255
-      // b = map(b, 0, 3, 0, 255);  //Map the 2-bit value to the range 0-255
-      m = cmd2 & 0x03;  //Mask with 0x03 (00000011)
+      r = (cmd2 >> 6) & 0b11;    //Extract bits 7-6 (RR)
+      g = (cmd2 >> 4) & 0b11;    //Extract bits 5-4 (GG)
+      b = (cmd2 >> 2) & 0b11;    //Extract bits 3-2 (BB)
+      r = map(r, 0, 3, 0, 255);  //Map the 2-bit value to the range 0-255
+      g = map(g, 0, 3, 0, 255);  //Map the 2-bit value to the range 0-255
+      b = map(b, 0, 3, 0, 255);  //Map the 2-bit value to the range 0-255
+      m = cmd2 & 0x03;           //Mask with 0x03 (00000011)
       if (m == 0) {
         LEDrainbow = false;
         LEDloop(true);
       }
-      // if (m == 1) {
-      //   strip.setPixelColor(0, strip.Color(r, g, b));
-      //   strip.show();
-      // }
+      if (m == 1) {
+        fill_solid(&(LEDs[0]), TotalLEDs, CRGB(r, g, b));
+        FastLED.show();
+      }
       if (m == 2)
-        if (strip.getPixelColor(0) == ColorIdle or LEDrainbow == 1) {  //Do not overwrite other modes OR if we want to sync the rainbow
-          LEDrainbow = true;                                           //Set rainbow mode
+        if (LEDs[0] == ColorIdle or LEDrainbow == 1) {  //Do not overwrite other modes OR if we want to sync the rainbow
+          LEDrainbow = true;                         //Set rainbow mode
           LEDloop(true);
         }
-#ifdef ESP32
+#ifdef SerialDebug
       Serial.println("r=" + String(r) + " g=" + String(g) + " b=" + String(b) + " m=" + String(m));
 #endif
       break;
@@ -208,10 +198,10 @@ uint8_t triggerAction(uint8_t cmd1, uint8_t cmd2) {
 }
 void DispenseStart() {
   LEDrainbow = false;
-  strip.setPixelColor(0, ColorDispencing);
-  strip.show();
+  fill_solid(&(LEDs[0]), TotalLEDs, ColorDispencing);
+  FastLED.show();
   digitalWrite(PDO_ValveAir, HIGH);
-  delay(DelayAir * 5);
+  delay(dispenserSettings.DelayAir * 5);
   digitalWrite(PDO_ValveFluid, HIGH);
 }
 void DispenseStop() {
@@ -224,32 +214,29 @@ void LEDloop(bool init) {
     static uint8_t gHue;
     if (init)
       gHue = 0;                           //Reset gHue offset
-    static unsigned long lastUpdate = 0;  // Every 40 milliseconds, increment the hue to create the rainbow effect
+    static unsigned long lastUpdate = 0;  //Every 40 milliseconds, increment the hue to create the rainbow effect
     if (millis() - lastUpdate >= 40) {
       lastUpdate = millis();
-      gHue++;  // Increment hue
-
-      // Create rainbow effect by setting color based on the current hue
-      uint32_t color = strip.ColorHSV(gHue * 256);  // Get RGB color from the HSV value
-      strip.setPixelColor(0, color);                // Set the color to the first LED
-      strip.show();                                 // Update the LED strip
+      gHue++;
+      fill_rainbow(&LEDs[0], 1, gHue, 1);
+      FastLED.show();
     }
   } else {
     if (init) {
       if (ImAdopted) {
-        strip.setPixelColor(0, ColorIdle);  // Set LED to red
+        fill_solid(&(LEDs[0]), TotalLEDs, ColorIdle);
       } else {
-        strip.setPixelColor(0, ColorWaitForAdoptionIdle);
+        fill_solid(&(LEDs[0]), TotalLEDs, ColorWaitForAdoptionIdle);
       }
-      strip.show();
+      FastLED.show();
     }
   }
 }
 void CheckAndGetSlotID() {
   if (bus.device_id() == PJON_NOT_ASSIGNED) {
-    if (strip.getPixelColor(0) != ColorGetID and strip.getPixelColor(0) != ColorDispencing) {  //If not yet desired color, but do not overwrite ColorDispencing
-      strip.setPixelColor(0, ColorGetID);
-      strip.show();
+    if (LEDs[0] != ColorGetID and LEDs[0] != ColorDispencing) {  //If not yet desired color, but do not overwrite ColorDispencing
+      fill_solid(&(LEDs[0]), TotalLEDs, ColorGetID);
+      FastLED.show();
     }
     delay(1);
     uint8_t ID1 = GetSlotID();
@@ -259,16 +246,16 @@ void CheckAndGetSlotID() {
     uint8_t ID3 = GetSlotID();
     if (ID1 > 0 && ID1 == ID2 && ID2 == ID3) {
       bus.set_id(ID1);
-      if (strip.getPixelColor(0) != ColorDispencing)  //Do not overwrite ColorDispencing
+      if (LEDs[0] != ColorDispencing)  //Do not overwrite ColorDispencing
         LEDloop(true);
       uint8_t BusSend[] = { ADOPT, bus.device_id() };  //Ask Primary for us to be adopted
       uint16_t result = bus.send(PrimaryID, &BusSend, sizeof(BusSend));
-#ifdef ESP32
+#ifdef SerialDebug
       Serial.println("SlotID recieved, I am " + String(bus.device_id()));
       if (result == PJON_FAIL) Serial.print("bus request fail =" + String(result));
 #endif
     }
-#ifdef ESP32
+#ifdef SerialDebug
     else
       Serial.println("Error in SlotID recieved, ID1=" + String(ID1) + " ID2=" + String(ID2) + " ID3=" + String(ID3));
 #endif
