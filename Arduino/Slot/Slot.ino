b265f0e4c3566f682dc39a4d4578ca1912e66488
JelleWho <1545223+jellewie@users.noreply.github.com>
2025-06-09 17:15:40 +0200

    Added note about burning bootloader

--- Arduino/Slot/Slot.ino
+++ Arduino/Slot/Slot.ino
@@ -2,7 +2,7 @@
   Program written by JelleWho https://github.com/jellewie
   Board: Attiny85
     Internal 8Mhz
-  
+  Do an "Butn bootloader" for each new Attiny85 you program to burn in the clock settings!
   This code is debugable on an ESP32, when the compiler detects you are compiling to an ESP it will change the code to debugging
 */
 
@@ -12,29 +12,25 @@ uint8_t SlotID = 1;  //Unique 1-byte ID (change for every slot for dispenser). M
 const uint8_t PDI_Slot_TXRX = 13;
 #else
 const uint8_t PDI_Slot_TXRX = 4;
+const uint8_t LED_BUILTIN = 3;
 #endif
 const uint8_t Delaypulses = 5;      //Below 3 seems to be too unstable
 const uint8_t DelayBeforeSend = 5;  //below 4 we start losing some pulses at the start
-const uint8_t DelayAfterSend = 20;  //Must be higher then lastPulseTime timeout of DISPENSER
+const uint8_t DelayAfterSend = 20;  //Must be higher then lastPulseTimeTimeout of DISPENSER
 void setup() {
   pinMode(PDI_Slot_TXRX, OUTPUT);
-#ifdef ESP32
   pinMode(LED_BUILTIN, OUTPUT);
-#else
+#ifdef ESP32
   SlotID = SlotID + 1;  //IDK seems the ATTiny always does not count the int, so just increase it to compensate
 #endif
 }
 void loop() {
-#ifdef ESP32
   digitalWrite(LED_BUILTIN, LOW);
-#endif
   digitalWrite(PDI_Slot_TXRX, HIGH);
   pinMode(PDI_Slot_TXRX, INPUT);
   delay(DelayAfterSend);
   while (digitalRead(PDI_Slot_TXRX) == HIGH) {}  //Wait until its is pulled low by a dispenser
-#ifdef ESP32
   digitalWrite(LED_BUILTIN, HIGH);
-#endif
   pinMode(PDI_Slot_TXRX, OUTPUT);
   delay(DelayBeforeSend);
   for (uint8_t i = 0; i < SlotID; i++) {  //Amount of pulses based on SlotID
